c9a394e183e43b885ce226e876e846279453ffc9
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-03-22 18:34:50 -0500

    Fetched list of characters from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import reactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import axios from 'axios';
 import Header from "./components/Header.js";
 
 export default function App() {
+  const [charaterForm, setCharacterForm] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      setCharacterForm(response.data.results) 
+    });
+  }, []);
+  useEffect(() =>{
+
+  }, [characterForm]);
+
+
   return (
     <main data-testid='app'>
       <Header />
